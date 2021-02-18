8efd1ae9d5377e0397b1bbbc54d4faaa65e5e30b
Jessica Mangiameli <jmangiam@asu.edu>
2021-02-18 12:04:01 -0600

    controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,6 @@ const usersRoutes = require('./user-routes');
 const thoughtsRoutes = require('./thought-routes');
 
 router.use('/users', usersRoutes);
-
 router.use('/thoughts', thoughtsRoutes);
 
 module.exports = router;
